5c74933b917ae5feda3eea727f3b5104805cbfc7
MyPetIsBear <alexandr.ledovskiy@gmail.com>
2013-10-24 22:48:33 +0400

    in work

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 from app.main.views import mod as mainModule
+from app.service.views import mod as serviceModule
+
 app.register_blueprint(mainModule)
+app.register_blueprint(serviceModule)
